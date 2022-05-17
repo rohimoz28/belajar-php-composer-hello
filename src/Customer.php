6b6bf2062d1d1d805d11bc331f70896f9b0d191d
rohimoz28 <rohimoz28@gmail.com>
2022-05-17 11:17:19 +0700

    Contoh Library Sederhana Composer

---
+++ src/Customer.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace ProgrammerZamanNow\Belajar;
+
+class Customer
+{
+  public function __construct(private string $name)
+  {
+  }
+
+  public function sayHello(string $name)
+  {
+    return "Hello $name my name is $this->name";
+  }
+}
